db62ffe5d5eb76794e37d76ab839feb84dad2fa6
elepampam <budisuryadarma0407@gmail.com>
2017-11-07 12:43:08 +0800

    adding bootstrap, jquery, popper

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import TodoList from './Components/TodoList'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App container">
           <TodoList/>        
       </div>
     );
